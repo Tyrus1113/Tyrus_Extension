fcab938f1fde5b20ab29242a59a340255de508c2
Tyrus <tyrusl@163.com>
2018-02-21 15:25:20 +0800

    git v0.1 Des: removeItem  sortNum  getMaxMinItem

--- Tyrus_ExtensionJS.js
+++ Tyrus_ExtensionJS.js
@@ -6,28 +6,27 @@ Array.prototype.removeItem = function (x) {
         if (this[i] == x) {
             this.splice(i,1)
             break
-        } else {
-            console.log('Tyrus_ExtensionJS_Error: ')
         }
     }
 }
 // 数字从大到小/从小到大排序
 Array.prototype.sortNum = function (k) {
+    // 若没有给定参数 则默认从小到大排序
     if (k == undefined) {k = false}
-    var newVal = 0
+    var n = 0
     for (var i = 0; i < this.length; i++) {
         for (var j = 0; j <= i; j++) {
             if (k == true) {
                 if (this[i] > this[j]) {
-                    newVal = this[i]
+                    n = this[i]
                     this[i] = this[j]
-                    this[j] = newVal
+                    this[j] = n
                 }
             } else {
                 if (this[i] < this[j]) {
-                    newVal = this[i]
+                    n = this[i]
                     this[i] = this[j]
-                    this[j] = newVal
+                    this[j] = n
                 }
             }
         }
@@ -37,29 +36,29 @@ Array.prototype.sortNum = function (k) {
 // 取数组中最大项/最小项
 Array.prototype.getMaxMinItem = function (k) {
     if (k == undefined) {k = false}
-    var newVal = 0
+    var n = 0
     for (var i = 0; i < this.length; i++) {
         if (k == true) {
-            if (this[i] > newVal) {
-                newVal = this[i]
+            if (this[i] > n) {
+                n = this[i]
             }
         } else {
-            if (this[i] < newVal) {
-                newVal = this[i]
+            if (this[i] < n) {
+                n = this[i]
             }
         }
     }
-    return newVal
+    return n
 }
 
-var numArr = [0,1,2,3,4,5,6,7]
+var numArr = [6,3,4,2,1,7,0,5]
 var strArr = ['a','b','c','d','e','f','g']
 
 console.log(numArr)
 // numArr.removeItem(2)
-// numArr.sortNum(true)
-console.log(numArr)
-console.log(numArr.getMaxMinItem(true))
+// numArr.sortNum()
+// console.log(numArr)
+console.log(numArr.getMaxMinItem())
 
 
 
