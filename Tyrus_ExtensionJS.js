e00e3c905fb3314dd6fb641279aa30674645a6b5
Tyrus <tyrusl@163.com>
2018-02-26 23:27:30 +0800

    git v0.1.5 Des: concatUniqueSort 数组合并 去重 排序  并添加日期排序功能

--- Tyrus_ExtensionJS.js
+++ Tyrus_ExtensionJS.js
@@ -123,18 +123,61 @@ Array.prototype.numberSection = function () {
 
 // 对象是否为空 true:空 | false:非空
 Object.prototype.isEmptyObj = function () {
-    var k = 'Tyrus_ExtensionJS Error: 不是对象类型'  
+    var e = 'Tyrus_ExtensionJS Error: 不是对象类型'  
     var s = Object.prototype.toString.call(this)
     if (s == '[object Object]') {
-        for (var key in this) {
-            if (this.hasOwnProperty(key)) {
+        for (var k in this) {
+            if (this.hasOwnProperty(k)) {
                 return false
             }
         }
     } else {
-        return k
+        return e
     }
     return true
+}
+
+// 数组合并 去重 排序
+Array.prototype.concatUniqueSort = function (a1,a2) {
+    if (arguments.length <= 0) {
+        return 'Tyrus_ExtensionJS Error: 缺少参数'
+    }
+    
+    // 先合并去重前两个数组
+    function concatUniqueFunc (r1,r2) {
+        // 深拷贝第一个数组 让原数组的值不会被改变
+        var n = r1.concat()
+        for (var i = 0; i < r2.length; i++) {
+            if (n.indexOf(r2[i]) === -1) {
+                // r2[i]到n中进行查找重复值 
+                // 返回-1则无重复
+                n.push(r2[i])
+            }
+        }
+        return n
+    }
+    var s = concatUniqueFunc(this,a1)
+    for (let i = 1; i < arguments.length; i++) {
+        // 从第二个参数开始循环执行合并去重
+        s = concatUniqueFunc(s,arguments[i])
+    }
+
+    var t = []
+    for (var i = 0; i < s.length; i++) {
+        t.push(s[i].replace('-',''))
+    }
+    console.log(t);
+
+
+    // 给sort方法添加排序规则
+    function sortFunc (a,b) { return a - b }
+
+    var nt = t.sort(sortFunc)
+    var nn = []
+    for (var i = 0; i < nt.length; i++) {
+        nn.push(nt[i].splice(5,0,'-')) // 错误出现在此行  尽快完成此方法
+    }
+    return nn
 }
 
 // 测试区 ------
@@ -144,7 +187,15 @@ var strArr = ['a','b','c','d','e','f','g']
 var strDate = "2018-02-22 12:11:00"
 var strTest = ' 去除 两边 空白   '
 var ar = [0,10,20,30,40,50]
+// var arr1 = [1,3,5,7,9]  
+// var arr2 = [2,1,6,5,10]  
+// var arr3 = [3,10,7,1,15]
+// var arr4 = [4,21,6,18,7]
+var arr1 = ['2017-01','2017-02','2017-03','2017-04','2017-07','2018-01']  
+var arr2 = ['2017-01','2017-03','2017-04','2017-05','2017-06']  
+var arr3 = ['2017-01','2017-02','2017-03','2017-04']
 console.log(objTest.isEmptyObj());
+console.log(arr1.concatUniqueSort(arr2,arr3));
 // console.log(Object.prototype.toString.call(numArr));
 // console.log(strDate.periodTime());
 // console.log(strTest)
