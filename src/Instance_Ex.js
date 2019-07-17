a8abea9b9e640257a6f32a1f0806867406d08cd2
lvlongxi <lvlongxi@smzdm.com>
2019-07-17 17:22:01 +0800

    v0.24.2 des: 移除测试代码 新增文本框

--- src/Instance_Ex.js
+++ src/Instance_Ex.js
@@ -46,18 +46,3 @@ window.addEventListener('scroll', () => {
 document.getElementById('formatterTest').onkeyup = function(e) {
     Ty.formatterToFixed(this)
 }
-
-// 截取小数点后几位
-// function clearNoNum(obj) {
-//     console.log(obj, obj.value)
-//     /* eslint no-useless-escape: "error" */
-//     obj.value = obj.value.replace(/[^\d.]/g, '') // 清除“数字”和“.”以外的字符
-//     obj.value = obj.value.replace(/\.{2,}/g, '.') // 只保留第一个. 清除多余的
-//     obj.value = obj.value.replace('.', '$#$').replace(/\./g, '').replace('$#$', '.')
-//     // eslint-disable-next-line no-useless-escape
-//     obj.value = obj.value.replace(/^(\-)*(\d+)\.(\d\d).*$/, '$1$2.$3') // 只能输入两个小数
-//     if (obj.value.indexOf('.') < 0 && obj.value !== '') { // 以上已经过滤，此处控制的是如果没有小数点，首位不能为类似于 01、02的金额
-//         obj.value = parseFloat(obj.value)
-//     }
-// }
-// clearNoNum()
